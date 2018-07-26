1f75e1f08f7399ebc8bdf0e9e81acf71baddf36c
GracieChristine <graciechristineli@gmail.com>
2018-07-25 18:01:43 -0600

    Adding new kata.

---
+++ 7-Kyu/Area of a Circle.js
@@ -0,0 +1,13 @@
+// Complete the function circleArea so that it will return the area of a circle with the given radius. Round the returned number to two decimal places (except for Haskell). If the radius is not positive or not a number, return false.
+//
+// Example:
+//
+// circleArea(-1485.86);    //returns false
+// circleArea(0);           //returns false
+// circleArea(43.2673);     //returns 5881.25
+// circleArea(68);          //returns 14526.72
+// circleArea("number");    //returns false
+
+let circleArea = function(radius) {
+  return (typeof radius != 'number' || radius < 1) ? false : parseFloat((radius * radius * Math.PI).toFixed(2));
+};
