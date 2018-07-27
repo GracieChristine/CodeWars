1c371a0ad8c99047dc733b77ccf5fa37c17084ea
GracieChristine <graciechristineli@gmail.com>
2018-07-26 22:27:59 -0600

    Adding new kata.

--- 7-Kyu/Array Series #6 - Minimum Steps.js
+++ 7-Kyu/Array Series #6 - Minimum Steps.js
@@ -24,7 +24,17 @@ function minimumSteps(numbers, value) {
   numbers = numbers.sort((a, b) => a - b);
   for (let i = 0; i < numbers.length; i++) {
     term += numbers[i];
-    if (term >= value) 
+    if (term >= value)
       return i;
     }
   }
+
+function minimumSteps(numbers, value) {
+  const nums = numbers.slice().sort((a, b) => a - b);
+  for (let i = 0, sum = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum >= value) {
+      return i;
+    }
+  }
+}
