924c0b8f4da2574444c57c67f6c6960f22f8d495
GracieChristine <graciechristineli@gmail.com>
2018-07-31 18:26:29 -0600

    Adding new kata.

---
+++ 6-Kyu/Format a string of names like 'Bart, Lisa & Maggie'.js
@@ -0,0 +1,45 @@
+// Given: an array containing hashes of names
+//
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+//
+// Example:
+//
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+//  returns 'Bart, Lisa & Maggie'
+//
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+//  returns 'Bart & Lisa'
+//
+// list([ {name: 'Bart'} ])
+//  returns 'Bart'
+//
+// list([])
+//  returns ''
+// Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+function list(names) {
+
+  let str = '';
+
+  if (names === undefined)
+    return str;
+
+  names.map((name, index) => {
+    return (index >= names.length - 2)
+      ? str += names[index]['name'] + ' & '
+      : str += names[index]['name'] + ', '
+  })
+  return str.slice(0, str.length - 2).trim();
+}
+
+function list(names) {
+  return names.reduce(function(prev, current, index, array) {
+    if (index === 0) {
+      return current.name;
+    } else if (index === array.length - 1) {
+      return prev + ' & ' + current.name;
+    } else {
+      return prev + ', ' + current.name;
+    }
+  }, '');
+}
