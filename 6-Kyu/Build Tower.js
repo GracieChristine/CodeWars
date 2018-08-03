2ce2e42e1e91c17ac47543fd64c65666458a40d5
GracieChristine <graciechristineli@gmail.com>
2018-08-03 17:13:14 -0600

    Adding difficult kata.

--- 6-Kyu/Build Tower.js
+++ 6-Kyu/Build Tower.js
@@ -45,3 +45,10 @@ function towerBuilder(nFloors) {
   }
   return arr;
 }
+
+function towerBuilder(n) {
+  return Array.from({length: n}, function(v, k) {
+    const spaces = ' '.repeat(n - k - 1);
+    return spaces + '*'.repeat(k + k + 1) + spaces;
+  });
+}
