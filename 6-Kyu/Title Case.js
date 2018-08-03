ca6fe13770343edf639512fffb2f0cf27201d97b
GracieChristine <graciechristineli@gmail.com>
2018-08-03 17:10:27 -0600

    Adding new kata.

--- 6-Kyu/Title Case.js
+++ 6-Kyu/Title Case.js
@@ -36,3 +36,15 @@ function titleCase(title, minorWords) {
 
   return titleArr.join(' ');
 }
+
+function titleCase(title, minorWords) {
+  var minorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+  return title.toLowerCase().split(' ').map(function(v, i) {
+    if(v != "" && ( (minorWords.indexOf(v) === -1) || i == 0)) {
+      v = v.split('');
+      v[0] = v[0].toUpperCase();
+      v = v.join('');
+    }
+    return v;
+  }).join(' ');
+}
