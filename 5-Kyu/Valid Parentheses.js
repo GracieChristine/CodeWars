3bc783a52468643fdfecb0876106a687b7febcbf
GracieChristine <graciechristineli@gmail.com>
2018-08-04 09:51:13 -0600

    Adding new kata.

---
+++ 5-Kyu/Valid Parentheses.js
@@ -0,0 +1,19 @@
+// Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+//
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= input.length <= 100
+//
+// You may assume that the input string will only contain opening and closing parenthesis and will not be an empty string.
+
+function validParentheses(parens){
+  let count = 0;
+  for (let i = 0 ; i < parens.length && count >= 0; i++) {
+    count += (parens[i] == '(') ? 1 : -1;
+  }
+  return (count == 0);
+}
