8d76e412a86a2695a8f610b3aca3d72688838750
GracieChristine <graciechristineli@gmail.com>
2018-08-06 05:52:03 -0600

    Adding new Java kata.

--- 5-Kyu/Product of consecutive Fib numbers.js
+++ 5-Kyu/Product of consecutive Fib numbers.js
@@ -32,3 +32,16 @@ function productFib(prod){
   while(a * b < prod) [a, b] = [b, a + b];
   return [a, b, a * b === prod];
 }
+
+
+function productFib(prod) {
+  var n = 0;
+  var nPlus = 1;
+  while (n * nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [
+    n, nPlus, n * nPlus === prod
+  ];
+}
