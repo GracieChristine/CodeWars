6ef22cb2c07c1bf05f019aa8f880e8bbd9d36d93
GracieChristine <graciechristineli@gmail.com>
2018-08-06 18:00:13 -0600

    Adding new kata.

---
+++ 8-Kyu/Sum of positive.java
@@ -0,0 +1,17 @@
+public class Positive{
+  public static int sum(int[] arr) {
+    int sum = 0;
+    for (int i : arr)
+      if (i > 0) {
+        sum += i;
+      }
+  return sum;
+  }
+}
+
+// import java.util.Arrays;
+// public class Positive{
+//   public static int sum(int[] arr){
+//     return Arrays.stream(arr).filter(v -> v > 0).sum();
+//   }
+// }
