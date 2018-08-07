bee23fce363841da9e3cd89fe54f46034a86d66e
GracieChristine <graciechristineli@gmail.com>
2018-08-07 17:11:17 -0600

    Adding new kata.

---
+++ Java/8-Kyu/Jenny_secret_message.java
@@ -0,0 +1,6 @@
+public class Jenny_secret_message {
+  public static String greet(String name) {
+    // return String.format("Hello, %s!", name);
+    return (name == "Johnny") ? "Hello, my love!" : "Hello, " + name + "!";
+  }
+}
