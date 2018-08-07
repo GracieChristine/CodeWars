3116b610c6b5ce104911c581b0c9530dd4fdd877
GracieChristine <graciechristineli@gmail.com>
2018-08-07 16:52:23 -0600

    Adding new kata.

---
+++ Java/8-Kyu/Convert_boolean_value.java
@@ -0,0 +1,16 @@
+// class Convert_boolean_value {
+//   public static String boolToWord(boolean b) {
+//     if (b) {
+//       return "Yes";
+//     }
+//     else {
+//       return "No";
+//     }
+//   }
+// }
+
+class Convert_boolean_value {
+  public static String boolToWord(boolean b) {
+    return (b) ? "Yes" : "No";
+  }
+}
