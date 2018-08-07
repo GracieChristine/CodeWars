d98b81258c677e56157735234bffb28df23a63e4
GracieChristine <graciechristineli@gmail.com>
2018-08-07 17:48:00 -0600

    Adding new kata.

---
+++ Java/8-Kyu/Convert_Number_String.java
@@ -0,0 +1,11 @@
+class Convert_Number_String {
+  public static String numberToString(int num) {
+    return Integer.toString(num);
+  }
+}
+
+// class Convert_Number_String {
+//   public static String numberToString(int num) {
+//     return String.valueOf(num);
+//   }
+// }
