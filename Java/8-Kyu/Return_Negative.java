043ff30b86c22febbc6136262c27ed9b6f521a03
GracieChristine <graciechristineli@gmail.com>
2018-08-06 18:18:56 -0600

    Adding new kata.

---
+++ Java/8-Kyu/Return_Negative.java
@@ -0,0 +1,16 @@
+public class Return_Negative {
+  public static int makeNegative(final int x) {
+    if (x < 0) {
+      return x;
+    }
+    else {
+      return -x;
+    }
+  }
+}
+
+// public class Kata {
+//   public static int makeNegative(final int x) {
+//       return (x < 0) ? x : -x;
+//   }
+// }
