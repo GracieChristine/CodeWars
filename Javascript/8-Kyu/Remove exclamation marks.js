742b457878ee549b9607b805ccceec56e20b26cc
GracieChristine <graciechristineli@gmail.com>
2018-08-08 20:10:30 -0600

    Adding new kata.

--- Javascript/8-Kyu/Remove exclamation marks.js
+++ Javascript/8-Kyu/Remove exclamation marks.js
@@ -3,3 +3,15 @@
 function removeExclamationMarks(s) {
   return s.split('!').join('');
 }
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/gi, '');
+}
+
+function removeExclamationMarks(s) {
+  var arr = s.split("");
+  arr = arr.filter(function(e) {
+    return e !== "!";
+  })
+  return arr.join("");
+}
