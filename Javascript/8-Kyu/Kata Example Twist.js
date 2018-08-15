0cbf98387a55d4883bf162bfc6897cad95d496f0
GracieChristine <graciechristineli@gmail.com>
2018-08-14 21:29:22 -0600

    Adding new kata.

--- Javascript/8-Kyu/Kata Example Twist.js
+++ Javascript/8-Kyu/Kata Example Twist.js
@@ -2,10 +2,16 @@
 //
 // Add the value "codewars" to the array websites/Websites 1,000 times.
 //
-// var websites = []
-
+// let websites = []
 
 let websites = [];
 for (let i = 0; i < 1000; i++) {
   websites.push('codewars');
 }
+
+let websites = [];
+while (websites.length < 1000) {
+  websites.push("codewars")
+}
+
+let websites = new Array(1000).fill("codewars");
