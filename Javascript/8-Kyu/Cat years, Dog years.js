09655c58869475988efa79afefd4fbc1df2df2ab
GracieChristine <graciechristineli@gmail.com>
2018-08-14 21:17:33 -0600

    Adding new kata.

--- Javascript/8-Kyu/Cat years, Dog years.js
+++ Javascript/8-Kyu/Cat years, Dog years.js
@@ -22,3 +22,15 @@ let humanYearsCatYearsDogYears = (humanYears) => {
     humanYears, (humanYears - 1 ? 16 : 11) + 4 * humanYears, (humanYears - 1 ? 14 : 10) + 5 * humanYears
   ];
 }
+
+function humanYearsCatYearsDogYears(humanYears) {
+  switch (humanYears) {
+    case 1:
+      return [1, 15, 15]
+    case 2:
+      return [2, 24, 24];
+    default:
+      return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24 ];
+  }
+
+}
