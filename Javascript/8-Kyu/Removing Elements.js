c1a9d3b4198a07f37636ebaa45a095fae3ce4d85
GracieChristine <graciechristineli@gmail.com>
2018-08-14 20:56:46 -0600

    Adding new kata.

--- Javascript/8-Kyu/Removing Elements.js
+++ Javascript/8-Kyu/Removing Elements.js
@@ -10,3 +10,17 @@ let removeEveryOther = (arr) => {
     return index % 2 === 0;
   })
 }
+
+function removeEveryOther(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+function removeEveryOther(arr) {
+  return arr.filter(function(elem, index) {
+    return index % 2 === 0;
+  });
+}
