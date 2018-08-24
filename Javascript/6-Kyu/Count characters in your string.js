a8a07e217aad560a96e14366ea334f681d6b0115
GracieChristine <graciechristineli@gmail.com>
2018-08-23 20:14:18 -0600

    Adding new kata.

---
+++ Javascript/6-Kyu/Count characters in your string.js
@@ -0,0 +1,15 @@
+// The main idea is to count all the occuring characters(UTF-8) in string. If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+//
+// What if the string is empty ? Then the result should be empty object literal { }
+
+function count(string) {
+  let result = {};
+  for (let i = 0; i < string.length; i++) {
+    if (result[string[i]] == undefined) {
+      result[string[i]] = 1;
+    } else {
+      result[string[i]] += 1;
+    }
+  }
+  return result;
+}
--- Javascript/6-Kyu/Detect Pangram.js
+++ Javascript/6-Kyu/Detect Pangram.js
@@ -17,3 +17,14 @@ let isPangram = (str) => {
   }
   return result;
 }
+
+function isPangram(string) {
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x) {
+    return string.indexOf(x) !== -1;
+  });
+}
+
+function isPangram(string) {
+  return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+}
