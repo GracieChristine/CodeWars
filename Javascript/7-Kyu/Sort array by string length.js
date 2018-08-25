34eaa26319a8f72f61c71decc3699ae26147baee
GracieChristine <graciechristineli@gmail.com>
2018-08-24 19:37:53 -0600

    Adding new kata.

--- Javascript/7-Kyu/Sort array by string length.js
+++ Javascript/7-Kyu/Sort array by string length.js
@@ -11,6 +11,10 @@
 // All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 
 function sortByLength(array) {
+  return array.sort((a, b) => a.length - b.length);
+};
+
+function sortByLength(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
       let temp = array[j]
