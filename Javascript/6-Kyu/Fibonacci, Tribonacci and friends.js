8887d52957f279f444456f6a3e6043c9605789d7
GracieChristine <graciechristineli@gmail.com>
2018-08-25 17:28:37 -0600

    Adding new kata.

--- Javascript/6-Kyu/Fibonacci, Tribonacci and friends.js
+++ Javascript/6-Kyu/Fibonacci, Tribonacci and friends.js
@@ -21,3 +21,10 @@ function Xbonacci(signature, n) {
 
   return signature.slice(0, n);
 }
+
+const Xbonacci = (sig, n) => {
+  let len = sig.length;
+  for (let i = len; i < n; i++)
+    sig[i] = sig.slice(i - len).reduce((a, b) => a + b);
+  return sig.slice(0, n);
+}
