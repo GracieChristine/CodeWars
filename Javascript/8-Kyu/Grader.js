886689b26021bc76dbb4283a12862fd886d1a78b
GracieChristine <graciechristineli@gmail.com>
2018-08-26 11:47:54 -0600

    Adding new kata.

--- Javascript/8-Kyu/Grader.js
+++ Javascript/8-Kyu/Grader.js
@@ -14,5 +14,38 @@
 // grader(0.75) == 'C'
 
 function grader(score) {
-  return (score > 1 || score < 0.6) ? "F" : (score >= 0.9) ? "A" : (score >= 0.8) ? "B" : (score >= 0.7) ? "C" : "D";
+  return (score > 1 || score < 0.6)
+    ? "F"
+    : (score >= 0.9)
+      ? "A"
+      : (score >= 0.8)
+        ? "B"
+        : (score >= 0.7)
+          ? "C"
+          : "D";
 }
+
+function grader(score) {
+  if (score > 1 || score < 0.6)
+    return 'F'
+  if (score < 0.7)
+    return 'D'
+  if (score < 0.8)
+    return 'C'
+  if (score < 0.9)
+    return 'B'
+  return 'A'
+}
+
+function grader(score) {
+  if (score > 1 || score < 0.6)
+    return 'F';
+  else if (score >= 0.9)
+    return 'A';
+  else if (score >= 0.8)
+    return 'B';
+  else if (score >= 0.7)
+    return 'C';
+  else if (score >= 0.6)
+    return 'D';
+  }
